d93ed7c4c2ad2a742df0d91c0dc52e056a938bdc
mybits <paulaproxy@gmail.com>
2014-12-24 22:22:17 +0000

    Add tasks with a form

--- simple-todos.js
+++ simple-todos.js
@@ -5,7 +5,27 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      // Show newest task first
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent defalut form submit
+      return false;
     }
   });
 }
